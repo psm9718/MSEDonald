1c204559b0f6d8b4ef8cda01edd6e99002be1ebf
psm9718 <psm9718@gmail.com>
2023-03-24 14:31:29 +0900

    chore: 서버 초기 세팅

---
+++ be/MseDonald/src/test/java/com/example/msedonald/MseDonaldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msedonald;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MseDonaldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
